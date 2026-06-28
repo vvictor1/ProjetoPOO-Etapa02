44e3ce53e455050d3a00951cbbf2139d195e9f8c
henrique.matheus <henriquematheussilva.21@gmail.com>
2026-06-28 20:44:44 -0300

    [Matheus] Refatoração: Implementação de Set para controle de mesas no Restaurante

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -14,3 +14,10 @@ private Map<Integer, Produto> mapaProdutos = new HashMap<>();
 public void cadastrarProduto(int id, String nome, double preco) {
     mapaProdutos.put(id, new Produto(id, nome, preco));
 }
+
+// Adicione no topo do Restaurante.java
+private Set<Integer> mesas = new HashSet<>();
+
+public void adicionarMesa(int numero) {
+    mesas.add(numero);
+}
